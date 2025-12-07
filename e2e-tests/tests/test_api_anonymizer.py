cfe35a135bda1b54cba5a240323578b4810e6714
Ryan Hastie <231561729+ryanhastie5890@users.noreply.github.com>
2025-12-07 23:49:19 +0000

    m

--- e2e-tests/tests/test_api_anonymizer.py
+++ e2e-tests/tests/test_api_anonymizer.py
@@ -404,42 +404,41 @@ def test_overlapping_keep_both():
 
 @pytest.mark.api
 def test_given_anonymize_called_with_genz_then_expected_valid_response_returned():
-    request_body = """{
-    "text": "Please contact Emily Carter at 734-555-9284 if you have questions about the workshop registration.",
-    "anonymizers": {
-        "DEFAULT": { 
-            "type": "genz", 
-            "operator_name": "genz", 
-            "entity_type": "DEFAULT" 
-        },
-        "PHONE_NUMBER": { 
-            "type": "genz", 
-            "operator_name": "genz", 
-            "entity_type": "PHONE_NUMBER" 
-        }
-    },
-    "analyzer_results": [
-        { 
-            "start": 15, 
-            "end": 27, 
-            "score": 0.3, 
-            "entity_type": "PERSON" 
-        },
-        { 
-            "start": 31, 
-            "end": 43, 
-            "score": 0.95, 
-            "entity_type": "PHONE_NUMBER" 
-        }
-    ]
-}
 
-"""
+    request_body = {
+        "text": "Please contact Emily Carter at 734-555-9284 if you have questions about the workshop registration.",
+        "anonymizers": {
+            "DEFAULT": { 
+                "type": "genz", 
+                "operator_name": "genz", 
+                "entity_type": "DEFAULT"
+            },
+            "PHONE_NUMBER": { 
+                "type": "genz", 
+                "operator_name": "genz", 
+                "entity_type": "PHONE_NUMBER"
+            }
+        },
+        "analyzer_results": [
+            { 
+                "start": 15, 
+                "end": 27, 
+                "score": 0.3, 
+                "entity_type": "PERSON"
+            },
+            { 
+                "start": 31, 
+                "end": 43, 
+                "score": 0.95, 
+                "entity_type": "PHONE_NUMBER"
+            }
+        ]
+    }
 
 
     response_status, response_content = anonymizeGenZ(request_body)
 
-   
-
+    
     assert response_status == 200
+
  
